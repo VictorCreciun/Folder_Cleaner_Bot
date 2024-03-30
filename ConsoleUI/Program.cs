7aac8372d5a3d7418e36aff55dbeace57a53e882
victo <victo@VITEK>
2024-03-30 15:46:59 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"D:\Temp";
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{file} : {fileInfo.LastAccessTime}");
+
+                if (fileInfo.LastAccessTime < currentTime.AddMinutes(-5))
+                {
+                    Console.WriteLine($"Deleting file {file}");
+                    File.Delete(file);
+                }
+
+            }
+        }
+    }
+}
